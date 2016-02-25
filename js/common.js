51afece01d4e01cb13be725140aab481951a5c0c
hongquangnguyen <hongquangnguyen92@gmail.com>
2016-02-25 11:12:41 +0700

    - Change appearance Add list task - Can add list of task - Show list of task and tasks by AngularJS

--- js/common.js
+++ js/common.js
@@ -5,9 +5,17 @@ $("#cancel-addList").click(function(e) {
     $newListInput.val('');
 });
 
-function toggler(divId) {
-    $("#" + divId).show();
-}
+jQuery(function($) {
+    $('.task').hover(function() {
+       $(this).children('.icon-pencil').addClass('glyphicon glyphicon-pencil');
+    },function() {
+       $(this).children('.icon-pencil').removeClass('glyphicon glyphicon-pencil');
+    });
+}); 
+
+//function showOff(divId) {
+//    $("." + divId).removeClass('glyphicon glyphicon-pencil');
+//}
 
 $(function () {
   $("#datepicker").datepicker({ 
@@ -15,3 +23,34 @@ $(function () {
         todayHighlight: true
   });
 });
+
+$('#modalAddList').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget);
+  var recipient = button.data('whatever');
+  var modal = $(this);
+  modal.find('.modal-title').text('Add New List');
+  modal.find('.modal-body input').val(recipient);
+})
+
+$('#closemodal').click(function() {
+    $('#savemodal').attr('disabled', 'disabled');
+});
+
+$('#savemodal').click(function() {
+    $('#modalAddList').modal('hide');
+    $('#savemodal').attr('disabled', 'disabled');
+});
+
+(function() {
+    $('#list-name').keyup(function() {
+        var empty = false;
+        if ($('#list-name').val() == '') {
+            empty = true;
+        }
+        if (empty) {
+            $('#savemodal').attr('disabled', 'disabled'); 
+        } else {
+            $('#savemodal').removeAttr('disabled'); 
+        }
+    });
+})()
