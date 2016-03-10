99fe2dfc2f81296cf0f807cfdfb39a83657f34e4
hongquangnguyen <hongquangnguyen92@gmail.com>
2016-03-10 22:04:22 +0700

    Update

--- js/app.js
+++ js/app.js
@@ -1,11 +1,6 @@
 var app = angular.module('tasks', ["pageslide-directive", "jsonService", "ui.router", "ngAnimate", "ui.bootstrap"]);
 
 angular.module('jsonService', ['ngResource'])
-.factory('ItemService', function($resource) { 
-    return $resource('js/json/listTasks.json',{ }, {
-        getData: {method:'GET', isArray: false}
-    });  
-})
 .factory('LoginService', function($resource) {
     return $resource('js/json/users.json',{ }, {
         getData: {method:'GET', isArray: false}
@@ -41,54 +36,41 @@ app.directive("datepicker", function () {
     };
 });
 
-app.controller('TasksController', function($scope, $rootScope, $window, $location, ItemService, LoginService, $uibModal){
-    ItemService.get(function(data){
-        $rootScope.listTasks = data.collections;
-    });
-//    LoginService.get(function(data){
-//            $rootScope.listUsers = data.listTask;
-//            $scope.lengthListUser = $scope.listUsers.length;
-//    });
-//    $scope.loginCheck = function(){
-//        // check id and password
-//       
-//        for(var i =0; i<$rootScope.listUsers.length; i++)
-//            if($rootScope.listUsers[i].userId == $scope.userId && $rootScope.listUsers[i].passWord == $scope.userPass)
-//            {
-//                
-//                $scope.currentUserName = $scope.listUsers[i].userName;
-//                $scope.currentUserId = $scope.listUsers[i].userId;
-//                //var user = [$scope.currentUserId, $scope.currentUserName];
-//                $scope.firstNameLetter = $scope.currentUserName.substring(0,1);
-//                localStorage.setItem("idUser", $scope.currentUserId);
-//                $location.path("html/to_do_page");
-//            }
-//    };
-//    $scope.loginCheck = function(){
-//        $location.path("html/to_do_page");
-//    }
-//    
+app.controller('TasksController', function($scope, $rootScope, $window, $location, LoginService, $uibModal){
+      
     LoginService.get(function(data){
         $rootScope.idUser = localStorage.getItem('idUser');
-        if($rootScope.idUser!=null){
-            $rootScope.listUsers = data.listTask;
-            $scope.lengthListUser = $scope.listUsers.length;
-            for(var i =0; i<$scope.lengthListUser; i++)
+        if($scope.idUser!=null){
+             
+            $scope.listUsers = data.listTask;
+            for(var i =0; i<$scope.listUsers.length; i++)
             {
-                if($rootScope.idUser == $rootScope.listUsers[i].userId )
-                    $rootScope.userCurrent = $scope.listUsers[i];
+                if($scope.idUser == $scope.listUsers[i].userId )
+                {
+                    $scope.userCurrent = $scope.listUsers[i];
+                    $rootScope.listTasks = $scope.userCurrent.collection;
+                }
             }
-        console.log($rootScope.userCurrent);
-        $rootScope.firstNameLetter = $rootScope.userCurrent.userName.substring(0,1);
-        $rootScope.fullName = $rootScope.userCurrent.userName;
+
+        $rootScope.firstNameLetter = $scope.userCurrent.userName.substring(0,1);
+        $rootScope.fullName = $scope.userCurrent.userName;
+        $scope.listTaskId = $scope.listTasks.taskListId;
+            // chosen list
+            $rootScope.valueIndex = 0;
+            for(var i=0; i<$scope.listTasks.length; i++)
+            {
+                if($rootScope.listTaskId == $scope.listTasks[i].taskListId)
+                    $rootScope.valueIndex = i;
+            }
+            // number of list task
+            $rootScope.numberOfList = 0;
+            for(var i=0; i<$scope.listUsers.length; i++)
+                for(var j=0; j<$scope.listUsers[i].collection.length; j++)
+                    $rootScope.numberOfList = $rootScope.numberOfList +1;
+        
         }
+        
     });
-//    
-//    $scope.loginCheck = function() {
-//      console.log("hello");
-//        $location.path("html/to_do_page");
-//
-//    };
     
     $scope.items = ['item1', 'item2', 'item3'];
 
@@ -109,7 +91,7 @@ app.controller('TasksController', function($scope, $rootScope, $window, $locatio
     };
     
     $scope.openEdit = function (size) {
-        $rootScope.editName = $rootScope.listTasks[$rootScope.valueIndex].nameListTask;
+        $rootScope.editName = $scope.listTasks[$scope.valueIndex].nameListTask;
         var modalInstance = $uibModal.open({
             animation: $scope.animationsEnabled,
             templateUrl: 'myModalEdit.html',
@@ -132,7 +114,6 @@ app.controller('TasksController', function($scope, $rootScope, $window, $locatio
     $scope.a.reminderTime = "";
     $scope.a.itemStatusTask = "inprogress";
     $scope.a.indexTask = "";
-    $rootScope.valueIndex = 0;
     $scope.view_tab = "tabInprogress";
     
     var today = new Date();
@@ -157,34 +138,40 @@ app.controller('TasksController', function($scope, $rootScope, $window, $locatio
     // Choose task list
     $scope.choose = function(indexCurrent){
         var x = indexCurrent;
-        $rootScope.valueIndex = x;
+        $rootScope.listTaskId = x;
         $rootScope.checkedright = false;
         $rootScope.checkedleft = false;
         
+        // chosen list
+        for(var i=0; i<$scope.listTasks.length; i++)
+        {
+            if($rootScope.listTaskId == $scope.listTasks[i].taskListId)
+                $rootScope.valueIndex = i;
+        }
+        
+        // due today
         if(x==1){
-            for(var i=0; i<$rootScope.listTasks.length; i++)
+            for(var i=0; i<$scope.listTasks.length; i++)
             {
                 if(i!=1)
                 {
-                    for(var j=0; j<$rootScope.listTasks[i].contentTask.length; j++)
+                    for(var j=0; j<$scope.listTasks[i].contentTask.length; j++)
                     {
-                        var todayDueDate = $rootScope.listTasks[i].contentTask[j].dueDay;
+                        var todayDueDate = $scope.listTasks[i].contentTask[j].dueDay;
                         if(todayDue==todayDueDate)
                         {
 
-                            if($rootScope.listTasks[1].contentTask.length==0)
-                                $rootScope.listTasks[1].contentTask.push( $rootScope.listTasks[i].contentTask[j]);
+                            if($scope.listTasks[1].contentTask.length==0)
+                                $scope.listTasks[1].contentTask.push( $scope.listTasks[i].contentTask[j]);
                             else{
-                                for(var z=0; z<$rootScope.listTasks[1].contentTask.length; z++)
+                                for(var z=0; z<$scope.listTasks[1].contentTask.length; z++)
                                 {
-                                    console.log($rootScope.listTasks[i].contentTask[j].taskId);
-                                    if($rootScope.listTasks[1].contentTask[z].taskId == $rootScope.listTasks[i].contentTask[j].taskId){
+                                    if($scope.listTasks[1].contentTask[z].taskId == $scope.listTasks[i].contentTask[j].taskId){
                                        break;
                                        }
-                                    else if($rootScope.listTasks[1].contentTask[z].taskId != $rootScope.listTasks[i].contentTask[j].taskId && ($rootScope.listTasks[1].contentTask.length-1) == z)
+                                    else if($scope.listTasks[1].contentTask[z].taskId != $scope.listTasks[i].contentTask[j].taskId && ($scope.listTasks[1].contentTask.length-1) == z)
                                     {
-                                        console.log("hello");
-                                        $rootScope.listTasks[1].contentTask.push( $rootScope.listTasks[i].contentTask[j]);  
+                                        $scope.listTasks[1].contentTask.push( $scope.listTasks[i].contentTask[j]);  
                                         break;
                                     }
                                 }
@@ -194,36 +181,36 @@ app.controller('TasksController', function($scope, $rootScope, $window, $locatio
                 }
             }
         }
-        if($rootScope.listTasks[1].contentTask.length > 0){
-            for(var z=0; z<$rootScope.listTasks[1].contentTask.length; z++)
+        if($scope.listTasks[1].contentTask.length > 0){
+            for(var z=0; z<$scope.listTasks[1].contentTask.length; z++)
             {
-                var dueDate = $rootScope.listTasks[1].contentTask[z].dueDay;
+                var dueDate = $scope.listTasks[1].contentTask[z].dueDay;
                 if(todayDue!=dueDate)
-                    $rootScope.listTasks[1].contentTask.pop($rootScope.listTasks[1].contentTask[z]);
+                    $scope.listTasks[1].contentTask.pop($scope.listTasks[1].contentTask[z]);
             }
         }
     };
     
     // Add new task to list
     $scope.addTask = function(indexCurrent) {
-        for(var i=0; i<$rootScope.listTasks.length; i++){
-            for(var j=0; j<$rootScope.listTasks[i].contentTask.length; j++){
-                $scope.numberTasks = $scope.numberTasks +1;
-            }
+        // number of tasks
+            for(var i=0; i<$scope.listUsers.length; i++)
+                for(var j=0; j<$scope.listUsers[i].collection.length; j++)
+                    for(var z=0; z<$scope.listUsers[i].collection[j].contentTask.length; z++)
+                        $scope.numberTasks = $scope.numberTasks +1;
+        if($scope.a.itemNameTask!=null && $scope.a.itemNameTask!=""){
+            var jsonStr = $scope.listTasks[indexCurrent].contentTask;
+            var new_obj = {
+                "nameTask": $scope.a.itemNameTask,
+                "dueDay": $scope.a.itemDueDate,
+                "statusTask": "inprogress",
+                "taskId": $scope.numberTasks,
+                "checkedTask": false
+            };
+            jsonStr.push( new_obj );
         }
-        console.log($scope.numberTasks);
-        var jsonStr = $rootScope.listTasks[indexCurrent].contentTask;
-            //console.log(abc);
-        var new_obj = {
-            "nameTask": $scope.a.itemNameTask,
-            "dueDay": $scope.a.itemDueDate,
-            "statusTask": "inprogress",
-            "taskId": $scope.numberTasks,
-            "checkedTask": false
-        };
-        $scope.numberTasks = 0;
-        jsonStr.push( new_obj );
         
+        $scope.numberTasks = 0;
         // Clear input fields after push
         $scope.a.itemNameTask = "";
         $scope.a.itemDueDate = "";
@@ -232,7 +219,7 @@ app.controller('TasksController', function($scope, $rootScope, $window, $locatio
     // Rename task
     $scope.renameTask = function(indexCurrent){
         if($scope.a.itemRenameTask!=null && $scope.a.itemRenameTask!=""){
-            $rootScope.listTasks[$rootScope.valueIndex].contentTask[indexCurrent].nameTask = $scope.a.itemRenameTask;
+            $scope.listTasks[$scope.valueIndex].contentTask[indexCurrent].nameTask = $scope.a.itemRenameTask;
         }
         
         // Clear input fields after push
@@ -268,25 +255,24 @@ app.controller('TasksController', function($scope, $rootScope, $window, $locatio
     
     // Change status task
     $scope.changeStatusTask = function(indexCurrent){
-        for(var i=0; i<$rootScope.listTasks[$rootScope.valueIndex].contentTask.length; i++){
-            if($rootScope.listTasks[$rootScope.valueIndex].contentTask[i].taskId==indexCurrent){
-                if($rootScope.listTasks[$rootScope.valueIndex].contentTask[i].statusTask=="inprogress")
+        for(var i=0; i<$scope.listTasks[$scope.valueIndex].contentTask.length; i++){
+            if($scope.listTasks[$scope.valueIndex].contentTask[i].taskId==indexCurrent){
+                if($scope.listTasks[$scope.valueIndex].contentTask[i].statusTask=="inprogress")
                 {
-                    $rootScope.listTasks[$rootScope.valueIndex].contentTask[i].statusTask = "completed";
-                    $rootScope.listTasks[$rootScope.valueIndex].contentTask[i].checkedTask = true;
+                    $scope.listTasks[$scope.valueIndex].contentTask[i].statusTask = "completed";
+                    $scope.listTasks[$scope.valueIndex].contentTask[i].checkedTask = true;
                 }
                 else
                 {
-                    $rootScope.listTasks[$rootScope.valueIndex].contentTask[i].statusTask = "inprogress";
-                    $rootScope.listTasks[$rootScope.valueIndex].contentTask[i].checkedTask = false;
+                    $scope.listTasks[$scope.valueIndex].contentTask[i].statusTask = "inprogress";
+                    $scope.listTasks[$scope.valueIndex].contentTask[i].checkedTask = false;
                 }
             }
         }
     }
  
     // Sign out
-    $scope.signOut = function(){
-        //console.log($rootScope.listTasks);    
+    $scope.signOut = function(){   
         $location.path("html/login");
     }
 
@@ -366,23 +352,29 @@ app.controller('ModalInstanceCtrl', function ($scope, $rootScope, $uibModalInsta
     $scope.addList = function() {
         if($scope.itemName!=null && $scope.itemName!=""){
             var jsonStr = $rootScope.listTasks;
-            var new_obj = {"nameListTask":$scope.itemName, "taskListId":$rootScope.listTasks.length+1, "contentTask" : []};
+            var new_obj = {"nameListTask":$scope.itemName, "taskListId":$rootScope.numberOfList+1, "contentTask" : []};
             jsonStr.push( new_obj );
             JSON.stringify(new_obj);
         }
         
+        $rootScope.numberOfList = $rootScope.numberOfList +1;
         // Clear input fields after push
         $scope.itemName = null;
         // Set value id to choose task list
-        $rootScope.valueIndex = $rootScope.listTasks.length-1;
+        $rootScope.listTaskId = new_obj.taskListId;
+        for(var i=0; i<$scope.listTasks.length; i++)
+        {
+            if($rootScope.listTaskId == $scope.listTasks[i].taskListId)
+                $rootScope.valueIndex = i;
+        }
         $uibModalInstance.close($scope.selected.item);
-        
+        $rootScope.checkedleft = false;
     };
     
     // Edit an item to the list
     $scope.editList = function() {
-        if($rootScope.editName!=null && $rootScope.editName!=""){
-            $rootScope.listTasks[$rootScope.valueIndex].nameListTask = $rootScope.editName;
+        if($scope.editName!=null && $scope.editName!=""){
+            $rootScope.listTasks[$scope.valueIndex].nameListTask = $scope.editName;
         }
         $uibModalInstance.close($scope.selected.item);
     }
@@ -406,6 +398,7 @@ app.controller('LoginController', function($scope, $rootScope, $window, LoginSer
     LoginService.get(function(data){
             $rootScope.listUsers = data.listTask;
             $scope.lengthListUser = $rootScope.listUsers.length;
+           
     });
     $scope.loginCheck = function(){
         // check id and password
@@ -420,5 +413,6 @@ app.controller('LoginController', function($scope, $rootScope, $window, LoginSer
                 localStorage.setItem("idUser", $scope.currentUserId);
                 $location.path("html/to_do_page");
             }
+        
     };
 }); 
--- js/directives/collections.js
+++ js/directives/collections.js
@@ -2,7 +2,7 @@ app.directive("collection", function() {
   return {
     restrict: 'E',
     template: 
-      '<div class="chooseTask" ng-repeat="itemS in listTasks" ng-click="choose($index)">'+
+      '<div class="chooseTask" ng-repeat="itemS in listTasks" ng-click="choose(itemS.taskListId)">'+
         '<div><h4 class="glyphicon glyphicon-tasks"></h4> {{itemS.nameListTask}}</div>'+
       '</div>'
   };
